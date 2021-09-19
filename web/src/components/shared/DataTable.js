cd11a9634842f3cff61f160ba32186efcb1e5f11
WonBeen Choi <50780281+ChoiWonBeen@users.noreply.github.com>
2021-09-19 13:30:00 +0000

    add table library

---
+++ web/src/components/shared/DataTable.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { DataGrid } from '@mui/x-data-grid';
+
+export default function DataTable({rows, columns}){
+  return (
+    <DataGrid 
+      rows={rows}
+      columns={columns}
+      pageSize={10}
+      rowsPerPageOptions={[10]}
+    />
+  )
+}
