692684acba26d5318a163607c94b300884b87058
KDM777 <69681273+KDM777@users.noreply.github.com>
2021-09-20 13:21:39 +0000

    add DateComponent

--- web/src/components/DateComponent.js
+++ web/src/components/DateComponent.js
@@ -6,80 +6,55 @@ import { Line } from 'react-chartjs-2';
 const Container = styled.div`
   margin-right: 3vw;
   margin-left: 7vw;
-  
 `
 
 const DateField = styled.div`
   width:56vw;
   margin-top:7vh;
-
+`
+const StartCalender = styled.div`
+  float:left;  
+  box-shadow: 0px 0px 0px 3px #000;
+`
+const EndCalender = styled.div`
+  margin-left:7vw;  
 `
 
 const Wrapper = styled.div`
-  margin-top:5vh;
+  margin-top:7vh;
   background-color:white;
-
 `
+
+const StartDate = styled.label`
+  color:#7000FF;
+`
+
 /*
 https://developer.mozilla.org/ko/docs/Web/HTML/Element/Input/date
 https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/Line.js
 https://www.chartjs.org/docs/latest/samples/line/line.html
 https://reactchartjs.github.io/react-chartjs-2/#/
+https://www.npmjs.com/package/react-datepicker
 */
 
-const data = {
-  labels: ['1', '2', '3', '4', '5', '6'],
-  datasets: [
-    {
-      label: '조식',
-      data: [12, 19, 3, 5, 2, 3],
-      fill: false,
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-    },
-    {
-      label: '중식',
-      data: [12, 19, 3, 6, 1, 0],
-      fill: false,
-      backgroundColor: 'rgb(54, 162, 235)',
-      borderColor: 'rgb(54, 162, 235)',
-    },
-    {
-      label: '석식',
-      data: [12, 10, 8, 5, 2, 3],
-      fill: false,
-      backgroundColor: 'rgb(255, 205, 86)',
-      borderColor: 'rgb(255, 205, 86)',
-    },
-  ],
-};
-
-const options = {
-  scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true,
-        },
-      },
-    ],
-  },
-};
-
-export default function DateComponent(){
+export default function DateComponent({data, options}){
   return (
     <Container>
       <DateField>
-        <label for="start">시작일:</label>
-        <input type="date" id="start" name="trip-start"
-          value="2021-01-01"
-          min="2021-01-01" max="2050-12-31">
-        </input>
-        <label for="start">종료일:</label>
-        <input type="date" id="start" name="trip-start"
-          value="2021-01-01"
-          min="2021-01-01" max="2050-12-31">
-        </input>
+        <StartCalender>
+          <StartDate for="start">시작일 </StartDate>
+          <input type="date" id="start" name="trip-start"
+            value="2021-01-01"
+            min="2021-01-01" max="2050-12-31">
+          </input>
+        </StartCalender>
+        <EndCalender>
+          <StartDate for="start">종료일</StartDate>
+          <input type="date" id="start" name="trip-start"
+            value="2021-01-01"
+            min="2021-01-01" max="2050-12-31">
+          </input>
+        </EndCalender>
       </DateField>
       <Wrapper>
         <Line data={data} options={options} />
