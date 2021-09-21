ed37b6b4083e165965f195d9c7456fd149815f37
WonBeen Choi <50780281+ChoiWonBeen@users.noreply.github.com>
2021-09-21 09:57:41 +0000

    add sidedish spec menulist box

--- web/src/components/SidedishSpecComponent.js
+++ web/src/components/SidedishSpecComponent.js
@@ -57,8 +57,43 @@ const Good = styled.span`
   color: #FF0000;
 `
 
+const Switch = styled.span`
+  float: right;
+  padding-right: 10px;
+`
 
-export default function SidedishSpecComponent({data}){
+const Label = styled.label`
+  font-size: 18px;
+  padding: 0 5px 0 20px;
+`
+
+const MenuList = styled.div`
+  padding: 10px 18px 0 18px;
+`
+
+const Menu = styled.div`
+  margin-top: 15px;
+`
+
+const Time = styled.div`
+  font-size: 15px;
+  font-style: italic;
+`
+
+const MenuDetail = styled.div`
+  border: 3px solid ${props => props.satisfy > 65 ? "#FF0000" : props.satisfy > 32 ? "#FF6534" : "#E7B70A"};
+  box-sizing: border-box;
+  border-radius: 5px;
+  padding: 17px;
+  font-size: 20px;
+`
+
+const MenuSatisfy = styled.span`
+  float: right;
+  color: ${props => props.satisfy > 65 ? "#FF0000" : props.satisfy > 32 ? "#FF6534" : "#E7B70A"};
+`
+
+export default function SidedishSpecComponent({data, order, setOrder}){
   return (
     <Container>
       <Box width={"40vw"}>
@@ -83,10 +118,34 @@ export default function SidedishSpecComponent({data}){
           병사들은 <Good>높은 만족도</Good>로 먹고있어요! <br/> 훌륭하게 조리중이예요!
         </Desc>
       </Box>
+
+
       <Box width={"21vw"}>
         <Head>
           사용된 메뉴
+          <Switch>
+            <Label htmlFor="lastest">최신순</Label>
+            <input type="radio" name="orderby" id="lastest" checked={order === 0} onChange={() => setOrder(0)}></input>
+
+            <Label htmlFor="satisfy">만족도순</Label>
+            <input type="radio" name="orderby" id="satisfy" checked={order === 1} onChange={() => setOrder(1)}></input>
+          </Switch>
         </Head>
+
+        <MenuList>
+          {data.menu_list.map((menu) => {
+            return (
+              <Menu>
+                <Time>{menu.lastest}</Time>
+                <MenuDetail satisfy={menu.satisfy}>
+                  {menu.name}
+
+                  <MenuSatisfy satisfy={menu.satisfy}>{menu.satisfy}%</MenuSatisfy>
+                </MenuDetail>
+              </Menu>
+            )
+          })}
+        </MenuList>
       </Box>
     </Container>
   )
