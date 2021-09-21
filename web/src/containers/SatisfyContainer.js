8194edd4187ac102e0e242010e0a553941b88cd9
yoonki <65451432+ryunki@users.noreply.github.com>
2021-09-21 07:30:08 +0000

    texts on image, PascalCase, image link issue

--- web/src/containers/SatisfyContainer.js
+++ web/src/containers/SatisfyContainer.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
+
 import SatisfyComponent from 'components/SatisfyComponent';
+
 
 export default function SatisfyContainer(){
   const [type, setType] = useState(0) // const [변수, set변수] = useState(기본값)
@@ -25,6 +27,44 @@ export default function SatisfyContainer(){
     }
   ]
 
+  const [taste, setSurvey] = useState(0) // const [변수, set변수] = useState(기본값)
+  const selectTaste = (idx) => {
+    setSurvey(idx)
+  }
+
+  const todayTaste = [
+    {taste: "짜다"},
+    {taste: "쓰다"},
+    {taste: "맵다"},
+    {taste: "달다"},
+    {taste: "시다"},
+    {taste: "싱겁다"},
+    {taste: "식었다"},
+    {taste: "양이 적다"}
+  ]
+  
+  const picLink=[
+    {
+      link: '../static/image/score_1.png',
+      name: "싫어요"
+    },
+    {
+      link: '../static/image/score_2.png',
+      name: "별로에요"
+    },
+    {
+      link: '../static/image/score_3.png',
+      name: "보통이에요"
+    },
+    {
+      link: '/..static/image/score_4.png',
+      name: "맛있어요"
+    },
+    {
+      link: '/..static/image/score_5.png',
+      name: "최고에요"
+    }
+  ]
 
   return (
     <SatisfyComponent 
@@ -32,7 +72,11 @@ export default function SatisfyContainer(){
       type={type} 
       selectType={selectType}
       selectTypeIdx={[1,2,3]} 
-      dataName={["싫어요","별로예요","보통이에요","맛있어요","최고예요"]}
+      todayTaste={todayTaste}
+      taste={taste}
+      selectTaste = {selectTaste}
+      selectSurveyIdx={[1,2,3,4,5,6,7,8]}
+      survey={picLink}
     />
   )
 }
