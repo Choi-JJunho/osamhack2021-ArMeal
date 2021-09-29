c567e5ada1cb4c323a8638659f2dfe6b0e38460e
Mr-jun <49794401+Choi-JJunho@users.noreply.github.com>
2021-09-29 12:57:50 +0000

    Fix Controller

--- cloud/src/main/java/com/api/controller/MenuController.java
+++ cloud/src/main/java/com/api/controller/MenuController.java
@@ -1,6 +1,8 @@
 package com.api.controller;
 
+import com.api.domain.DailyMeal;
 import com.api.domain.Menu;
+import com.api.mapper.MenuMapper;
 import com.api.service.MenuService;
 import java.sql.Timestamp;
 
@@ -36,10 +38,26 @@ public class MenuController {
         return new ResponseEntity<HashMap<Object,Object>>(menuService.updateMenuScore(menu.getName(), menu.getScore()),HttpStatus.OK);
     }
 */
-    // 자율메뉴 추가를 위한 addmenu
+    // 일반 메뉴데이터를 추가하기 위한 로직
     @RequestMapping(value = "/add/menu", method = RequestMethod.POST)
     public ResponseEntity addMenu(@RequestBody Menu menu) {
         return new ResponseEntity<HashMap<Object,Object>>(menuService.addMenu(menu.getName(), menu.getMenutype()),HttpStatus.OK);
     }
 
+    // 자율메뉴 추가를 위한 addmenu
+    @RequestMapping(value = "/add/dailymenu", method = RequestMethod.POST)
+    public ResponseEntity adddailyMenu(@RequestBody DailyMeal menu) {
+    return new ResponseEntity<HashMap<String, Object>>(menuService.addDailyMenu(menu),HttpStatus.OK);
+    }
+    // 해당 메뉴가 나온 날을 구한다.
+    @RequestMapping(value = "/get/date/menu/{id}", method = RequestMethod.GET)
+    public ResponseEntity getRecentDate (@PathVariable("id") long id) {
+        return new ResponseEntity<List<HashMap<String, Object>>>(menuService.getRecentDates(id), HttpStatus.OK);
+    }
+    // 해당 메뉴가 나온 날을 구한다.
+    @RequestMapping(value = "/get/menu/{id}", method = RequestMethod.GET)
+    public ResponseEntity getMenuById (@PathVariable("id") long id) {
+        return new ResponseEntity<Menu>(menuService.getMenuById(id), HttpStatus.OK);
+    }
+    
 }
