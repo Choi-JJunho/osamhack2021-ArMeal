017a3279d920485b3215d8c3968733fbcf26d9c6
WonBeen Choi <50780281+ChoiWonBeen@users.noreply.github.com>
2021-10-09 13:45:51 +0000

    combine get month date api

--- web/src/containers/DateContainer.js
+++ web/src/containers/DateContainer.js
@@ -7,6 +7,7 @@ export default function DateContainer(){
   const [order, setOrder] = useState(0);
   const { data } = useSelector(state => state.authReducer);
   const { allData } = useSelector(state => state.managementReducer);
+  const { month } = useSelector(state => state.managementReducer);
   const dispatch = useDispatch()
   const offset = new Date().getTimezoneOffset() * 60000;
   const todayStr = new Date(Date.now() - offset).toISOString().slice(0, 10);
@@ -22,6 +23,14 @@ export default function DateContainer(){
       [e.target.name]: e.target.value
     })
   }
+
+  useEffect(() => {
+    if(order === 1){
+      let newStart = new Date(Date.now() - offset - 1000*3600*24*30).toISOString().slice(0, 10);
+      setRange({...range, start: newStart})
+    }
+  }, [order])
+
   useEffect(() => {
     if(order === 0){
       dispatch(getManagementData({
@@ -29,6 +38,19 @@ export default function DateContainer(){
         start: range.start,
         end: range.end
       }))
+    }
+    if(order === 1){
+      let diff = range.end.slice(5, 7) - range.start.slice(5, 7)
+      console.log(diff)
+      let param = [];
+      for(let i = 0; i <= diff; i++){
+        param.push({
+          "group_id": data.group_id,
+          "year": range.start.slice(0, 4),
+          "month": Number(range.start.slice(5, 7)) + i,
+        })
+      }
+      dispatch(getMonthData(param))
     }
   },[dispatch, range, order, data])
   
@@ -75,6 +97,44 @@ export default function DateContainer(){
     }
   }, [allData.data])
 
+  useEffect(() => {
+    if(order === 1){
+      if(month.data){
+        let diff = range.end.slice(5, 7) - range.start.slice(5, 7)
+        let label = [];
+        for(let i = 0; i <= diff; i++){
+          label.push(range.start.slice(0, 5) + (Number(range.start.slice(5, 7)) + i))
+        }
+        setDateData({
+          labels: label,
+          datasets: [
+            {
+              label: '조식',
+              data: month.data[0].rating,
+              fill: false,
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+            },
+            {
+              label: '중식',
+              data: month.data[1].rating,
+              fill: false,
+              backgroundColor: 'rgb(54, 162, 235)',
+              borderColor: 'rgb(54, 162, 235)',
+            },
+            {
+              label: '석식',
+              data: month.data[2].rating,
+              fill: false,
+              backgroundColor: 'rgb(255, 205, 86)',
+              borderColor: 'rgb(255, 205, 86)',
+            },
+          ],
+        })
+      }
+    }
+  }, [month.data])
+
   const [dateData, setDateData] = useState({
     labels: ['2021-07-13', '2021-07-14', '3월', '4월', '5월', '6월'],
     datasets: [
@@ -99,16 +159,6 @@ export default function DateContainer(){
       ],
     },
   };
-
-  useEffect(() => {
-    if(order === 1){
-      dispatch(getManagementData({
-        group_id: data.group_id,
-        start: range.start,
-        end: range.end
-      }))
-    }
-  }, [dispatch, order, data, range])
 
   return (
     <DateComponent 
