80d53888147bf425ba233bf68430f00ba45f7668
WonBeen Choi <50780281+ChoiWonBeen@users.noreply.github.com>
2021-10-11 02:34:41 +0000

    add no-network keeping function

--- web/src/utils/asyncUtils.js
+++ web/src/utils/asyncUtils.js
@@ -63,6 +63,7 @@ export const handleAsyncActions = (type, key) => {
           ...state
         };
       case SUCCESS:
+        localStorage.clear();
         return {
           ...state,
           [key]: reducerUtils.success(action.payload)
