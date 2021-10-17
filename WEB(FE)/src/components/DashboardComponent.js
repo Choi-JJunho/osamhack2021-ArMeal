a2ce804e18ad30f8715dd0a3abd0155c1ae69a5d
yoonki <65451432+ryunki@users.noreply.github.com>
2021-10-17 09:56:37 +0000

    재시도

--- WEB(FE)/src/components/DashboardComponent.js
+++ WEB(FE)/src/components/DashboardComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components'
 import DoughnutChart from './shared/DoughnutChart';
-
+///////////////////////////////////////////
 const Container = styled.div`
   
 `
